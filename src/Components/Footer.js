cb67d76acf10a6f7e3aae6611c1c0c0bfc0c62dd
Randrews1331 <127296519+Randrews1331@users.noreply.github.com>
2023-11-10 16:28:34 +0000

    Footer
    more files

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './CSS/footer.css'
+const Footer = () => {
+return (
+    <footer>
+		<p>fjfgjdggfdsfgnfsdnjo</p>
+    </footer>
+  	   );
+};
+
+export default Footer;
