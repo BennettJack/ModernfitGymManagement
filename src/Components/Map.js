68082025f6842359b985d92129edac7800f236d5
Randrews1331 <127296519+Randrews1331@users.noreply.github.com>
2023-12-07 20:40:49 +0000

    added Map section to homepage
    I added the template for a new section will be modified to have the map + interactive later
    
    also changed text on main.js

---
+++ src/Components/Map.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../CSS/main.css'; // Import your CSS file containing styles
+import testVideo from './images/test.mp4'; // Import the video file
+
+const Main = () => {
+  return (
+    <h1>will be changed to an image of map and interrative</h1>
+
+  )
+};
+
+export default Main;
