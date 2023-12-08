dc5d52568f9cf37ace63f6bbe114c1372d63898c
Randrews1331 <127296519+Randrews1331@users.noreply.github.com>
2023-12-08 15:55:45 +0000

    re commit of prev
    re committing it since last time did not save or implemented the changes made

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,20 +1,18 @@
 import React from "react";
 import '../CSS/main.css'; // Import your CSS file containing styles
-import mapImage from './images/map.png'; // Import the image file
+import mapImage from './images/map.png'; // Import the video file
 
 const Main = () => {
   return (
-    
     <section>
-        <h1>will be changed to an image of map and interrative</h1>
-      <img>
-      <source src={mapImage} type="map/png" />
-      </img>
-      <div className="content">
-         <h1 className="cen-text">Find a gym near you!</h1>
+    <img>
+    <source src={mapImage} type="map/png" />
+    </img>
+    <div className="content">
+        <h1 className="cen-text">Find a gym near you!</h1>
+        {/* Additional content goes here */}
       </div>
     </section>
-
   )
 };
 
