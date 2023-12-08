5eb00e92dec7f663abfd95837d2d464940828ad3
Randrews1331 <127296519+Randrews1331@users.noreply.github.com>
2023-12-08 15:47:40 +0000

    small changes to map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,10 +1,19 @@
 import React from "react";
 import '../CSS/main.css'; // Import your CSS file containing styles
-import testVideo from './images/test.mp4'; // Import the video file
+import mapImage from './images/map.png'; // Import the image file
 
 const Main = () => {
   return (
-    <h1>will be changed to an image of map and interrative</h1>
+    
+    <section>
+        <h1>will be changed to an image of map and interrative</h1>
+      <img>
+      <source src={mapImage} type="map/png" />
+      </img>
+      <div className="content">
+         <h1 className="cen-text">Find a gym near you!</h1>
+      </div>
+    </section>
 
   )
 };
