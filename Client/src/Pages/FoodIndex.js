824080996780db57a26087272f9215670ce41221
SHU-Luke <c2015954@hallam.shu.ac.uk>
2023-12-30 14:24:46 +0000

    search updates

--- Client/src/Pages/FoodIndex.js
+++ Client/src/Pages/FoodIndex.js
@@ -27,33 +27,38 @@ const foodItems = data;
   }, []); */}
 
   const handleSearchChange = (e) => {
-    setSearch(e.target.value);
+    setSearch(e.target.value.toLowerCase());
   };
-
 const handleSearch = (e) => {
     e.preventDefault();
-    const results = data.filter((object) =>
-    object.name.includes(search)
-    );
-    setSearchResults(results);
-}
+    
+    if (search.trim() !== "") {
+      const results = data.filter((object) =>
+        object.name.toLowerCase().includes(search.toLowerCase())
+      );
+      setSearchResults(results);
+    } else {
+      setSearchResults([]);
+    }
+  };
     return ( 
       
         <div>
         <form onSubmit={handleSearch}>
-             <div className="formInput">
-                <label for="searchbox">Search</label>
+             <div className="foodItemContainerSearch">
+              <div className="searchContainer">
+                <label for="searchbox">Search: </label>
                 <input type="textbox" name="searchbox" id="searchbox" placeholder="Search Nutrition:" value={search} onChange={handleSearchChange}/> 
-                <div className="formInputButton">
-                    <button type="Submit" name="submit" id="submit">Search</button>
+                <button type="Submit" name="submit" id="submit">Search</button>
                 </div>
-            </div>
-            <div>
+            <div className="searchResults">
+              <p> Search Reults: </p>
                 <ul>
                 {searchResults.map((object) => (
                 <li>{object.name} {object.kcalper100g} {object.fatper100g} {object.sodiumper100g} {object.carbsper100g} {object.proteinper100g}</li>
                 ))}
                 </ul>
+            </div>
             </div>
         </form>
         </div>
